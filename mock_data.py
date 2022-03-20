f792cd4e7a2303bbe8dffc240a5b15ba9b25d810
ArturoExe <62444405+ArturoExe@users.noreply.github.com>
2022-03-20 15:02:41 -0700

    Add files via upload

---
+++ mock_data.py
@@ -0,0 +1,38 @@
+catalog=[
+    {
+        '_id':"I1",
+        'title':"Air Force 1",
+        'price':100,
+        'stock':10,
+        'image':"IMG-1.jpg"
+    },
+    {
+        '_id':"I2",
+        'title':"Air Force 2",
+        'price':100,
+        'stock':10,
+        'image':"IMG-2.jpg"
+    },
+    {
+        '_id':"I3",
+        'title':"Air Force 3",
+        'price':100,
+        'stock':10,
+        'image':"IMG-3.jpg"
+
+    },
+    {
+        '_id':"I4",
+        'title':"Air Force 4",
+        'price':100,
+        'stock':10,
+        'image':"IMG-4.jpg"
+
+    },{
+        '_id':"I5",
+        'title':"Air Force 5",
+        'price':100,
+        'stock':10,
+        'image':"IMG-5.jpg"
+    }
+]
--- server.py
+++ server.py
@@ -1,11 +1,16 @@
 #import flask library
-from flask import Flask
+from tkinter.messagebox import ABORT
+from flask import Flask,abort
+from mock_data import catalog
+import json
+
 
 #create the application "server"
 app= Flask("Server")
 
+ 
 ## App routes
-#Root route
+ #Root route
 @app.route("/")
 def home():
     return "Hello from flask"
@@ -14,6 +19,51 @@ def home():
 @app.route("/me")
 def about_me():
     return "Arturo Martinez Jr"
+
+
+################################################################################
+################################# API ENDPOINTS ################################
+################################# RETURN JSON ################################
+################################################################################
+
+#Retrive the catalog 
+@app.route("/api/products",methods=['GET'])
+def get_catalog():    
+    return json.dumps(catalog)
+
+#Post method test
+@app.route("/api/products",methods=['POST'])
+def save_catalog():
+    return "POST METHOD WORKING"
+
+#Count every product in the catalog
+@app.route("/api/products/count",methods=['GET'])
+def count_products():
+    
+    return json.dumps(len(catalog))
+
+#Get the sum of all products
+@app.route("/api/products/total",methods=['GET'])
+def total_products():
+
+    total=0
+    for x in catalog:
+        total=x['price']+total  
+
+    return json.dumps(total)
+
+#Get the product by ID <Sending the ID>
+@app.route("/api/products/<id>",methods=['GET'])
+def id_products(id):
+    for x in catalog:
+        if x['_id'] == id:
+            return json.dumps(x)
+
+    return abort(404,"There is no product with such ID")
+    
+
+   
+
 
 
 
